b4cf284c4d56338d7ed8c9efeecd7c296236d3c3
doyo91 <doyo_91@hotmail.com>
2020-07-22 15:37:28 +0200

    Updates

---
+++ precache-manifest.6846f62d5a3611c02505fc48cf06a4c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ae7a410e28f5008fea58601f6ecb53",
+    "url": "/flag-countries-react-mentor/index.html"
+  },
+  {
+    "revision": "c2b1b0f5da25f7d97b65",
+    "url": "/flag-countries-react-mentor/static/css/main.a0b04535.chunk.css"
+  },
+  {
+    "revision": "d78e6a06e82b7d2968a1",
+    "url": "/flag-countries-react-mentor/static/js/2.a97bcbf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flag-countries-react-mentor/static/js/2.a97bcbf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b1b0f5da25f7d97b65",
+    "url": "/flag-countries-react-mentor/static/js/main.8683c2ba.chunk.js"
+  },
+  {
+    "revision": "1df45becac33cd932e01",
+    "url": "/flag-countries-react-mentor/static/js/runtime-main.04441b7a.js"
+  }
+]);
